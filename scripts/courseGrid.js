4809c1f45c473dd2a694f97bfd0975b67aa4485f
Ahmed Samy <140907661+LaggyPlayz@users.noreply.github.com>
2025-04-29 19:55:38 +0300

    fix: icons not appearing properly
    <Head> Previously, the website would work well only while running with LiveServer.
    <Body> Now, you can see the same output with or without LiveServer.
      I also added a PageNotFound for some reason.
    <Foot> We have a PageNotFound Page & a functioning home page without a search bar yet...

--- scripts/courseGrid.js
+++ scripts/courseGrid.js
@@ -3,9 +3,9 @@ const cards = document.querySelectorAll(".courseCard");
 
 cards.forEach(card => {
     card.addEventListener("mouseover", () => {
-        card.style.cssText = "transform: translateY(-20px); transition: all 0.3s ease-out";
+        card.style.cssText = "transform: translateY(-20px); transition: all 0.3s ease-out; color: #8B30C4";
     });
     card.addEventListener("mouseleave", () => {
-        card.style.cssText = "transform: translateY(0); transition: all 0.3s ease-out";
+        card.style.cssText = "transform: translateY(0); transition: all 0.3s ease-out; color: black";
     });
 });
--- scripts/headerNav.js
+++ scripts/headerNav.js
@@ -1,3 +1,13 @@
 const items = document.querySelectorAll("header > nav > ul > li");
 
+// const navURLs = ["./../index.html", "./../index.html", ""];
+
+// items.forEach(item => {
+//     item.addEventListener("click", () => {
+//         // item.href = navURLs[items.keys[item]];
+//         window.location.href = navURLs[0];        
+//     });
+// });
+
 console.log(items);
+
