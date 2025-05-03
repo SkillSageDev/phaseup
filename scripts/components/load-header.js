f7d055e614f97d2260dcc61b6535f229304c59fc
Ahmed Samy <140907661+LaggyPlayz@users.noreply.github.com>
2025-05-03 04:57:24 +0300

    integration 0.8

--- scripts/components/load-header.js
+++ scripts/components/load-header.js
@@ -31,12 +31,13 @@ fetch("/pages/components/header.html")
 let obj = [
   {
     image: "/assets/course banners/Csharp course 2.svg",
-    alt: "c#",
-    title: "c# for .NET Developers",
+    alt: "C#",
+    title: "C# for .NET Developers",
     description:
       "Master C# Programming from A to Z. Dive deep into .NET, OOP, Clean Code, LINQ, WPF, Generics, Unit Testing, and more.",
     hours: "10h",
-    link: "#",
+    link: "./../overview.html",
+    category: "Game"
   },
   {
     image: "/assets/course banners/تعلم HTML في فيديو واحد 1.svg",
@@ -45,7 +46,8 @@ let obj = [
     description:
       "The modern JavaScript course for everyone! Master JavaScript with projects, challenges and theory. Many courses in one!",
     hours: "4h",
-    link: "#",
+    link: "./../overview.html",
+    category: "web"
   },
   {
     image: "/assets/course banners/كورس بايثون 1.svg",
@@ -54,7 +56,8 @@ let obj = [
     description:
       "Master Python by building 100 projects in 100 days. Learn data science, automation, build websites, games and apps!",
     hours: "56h 21m",
-    link: "#",
+    link: "./../overview.html",
+    category: "Data Science"
   },
   {
     image: "/assets/course banners/Unreal Engine 1.svg",
@@ -63,7 +66,8 @@ let obj = [
     description:
       "Code Your First Four Game Projects in Unreal Engine 5 with Blueprint Visual Scripting - From Beginner to Advanced!",
     hours: "41h 26m",
-    link: "#",
+    link: "./../overview.html",
+    category: "Game"
   },
 ];
 
