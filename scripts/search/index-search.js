18143b8183e0c5add70cd8795af74b5b997a4e8b
SkillSageDev <147807207+SkillSageDev@users.noreply.github.com>
2025-05-03 06:30:38 +0300

    feat: made the search work from every page.
    - Remove the load-header.js.
    - Add index-search.js to index.html
    - Add browse-serach.js to browse.html

--- scripts/search/index-search.js
+++ scripts/search/index-search.js
@@ -1,1 +1,10 @@
-let myInputSearch
+let searchForm = document.querySelector(".search");
+let searchInput = document.querySelector(".search-input");
+console.log(searchForm);
+console.log(searchInput);
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  window.localStorage.setItem("search", searchInput.value);
+  window.location.assign("/pages/search/browse.html");
+});
